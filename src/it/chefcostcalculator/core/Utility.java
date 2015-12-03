3609efc0b2ca4ac4321ffc609237acf7acccc0a9
Alessio R <raponialessio@hotmail.com>
2015-12-03 18:17:35 +0100

    Created utility class

---
+++ src/it/chefcostcalculator/core/Utility.java
@@ -0,0 +1,11 @@
+package it.chefcostcalculator.core;
+
+
+public abstract class Utility {
+	public static void printRecipe(Recipe recipe){
+		for(Dose o : recipe.getIngredientList()){
+			System.out.println(o.getIngredient().getIngredientName() + " " + o.getQuantity());
+		}
+	}
+
+}
